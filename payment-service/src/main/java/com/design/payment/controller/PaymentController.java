9c1bed9b3d8389396f3b8abbf3821655c8e64e7a
RAJESH <RAJESH@LAPTOP-CJBJ530B>
2021-11-22 00:07:21 +0530

    payment

---
+++ payment-service/src/main/java/com/design/payment/controller/PaymentController.java
@@ -0,0 +1,30 @@
+package com.design.payment.controller;
+
+import com.design.dto.PaymentRequestDTO;
+import com.design.dto.PaymentResponseDTO;
+import com.design.payment.service.PaymentService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("payment")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService service;
+
+    @PostMapping("/debit")
+    public PaymentResponseDTO debit(@RequestBody PaymentRequestDTO requestDTO){
+        return this.service.debit(requestDTO);
+    }
+
+    @PostMapping("/credit")
+    public void credit(@RequestBody PaymentRequestDTO requestDTO){
+        this.service.credit(requestDTO);
+    }
+
+}
